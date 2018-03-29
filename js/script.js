9d954d62146b0ee1d06849375e26631514402bcf
Overdoze <klg.panov@gmail.com>
2018-03-30 00:48:14 +0300

    filter test

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 $(function(){ "use strict";
 
     var $check = $(".js-check");
+    var commentsTable = [];
+
     if($check.length > 0){
 
         $check.on("click", function(e){
@@ -61,9 +63,56 @@ $(function(){ "use strict";
         
         //var comments = getComments(id,token);
         $.when(getComments(id,token)).done(function(comments){
-            for(var i=0; i<comments.data.length; i++){
-            console.log(comments.data[i].text);
+            if(comments.data.length == 0){
+                console.log("Nothing to wotk with");
+                return;
             }
+
+            $.ajax({
+                url: '/data/words.json',
+                dataType: 'json',
+                type: 'GET',
+                success: function( response ) {
+                    for(var i=0; i<comments.data.length; i++){
+                        console.log(comments.data[i].text);
+                        var words = comments.data[i].text.split(" ");
+                        for(var j=0; j<words.length; j++){
+                            if(response.indexOf(words[j]) > -1){
+                                addCommentToTable(comments.data[i]);
+                            }
+                        }
+                    }
+                    createTable();
+                },
+                error: function( jqXHR, textStatus, errorThrown ) {
+                    console.log( jqXHR, textStatus, errorThrown );
+                }
+            });
+
         });
+    }
+
+    function addCommentToTable(data){
+        var table_row = "<tr>" + 
+        "<td>" + data.id + "</td>" +
+        "<td>" + data.full_name + "</td>" +
+        "<td><a href='" + data.from.profile_picture + "'>Ссылка</a></td>" + 
+        "<td>" + data.text + "</td>" +
+        "</tr>";
+        commentsTable.push(table_row);
+    }
+
+    function createTable(){
+        if(commentsTable.length > 0){
+            $(".js-table").append("<table><tr>" +
+            "<th>ID комментария</th>" +
+            "<th>Имя комментатора</th>" +
+            "<th>Ссылка на комментарий</th>" +
+            "<th>Текст комментария</th>" +
+            "</tr></table>");
+            for(var i=0; i<commentsTable.length; i++){
+                $(".js-table table").append(commentsTable[i]);
+            }
+        }
     }
 });
